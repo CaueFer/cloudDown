548f90108491be152dfadbde00192290063fdafc
cauefer <cauefernandescaetano@gmail.com>
2025-05-11 01:25:45 -0300

    ` feat `: Remove toplogo e remaneja para footer

--- src/components/view/footer.tsx
+++ src/components/view/footer.tsx
@@ -1,14 +1,29 @@
-import { CloudDownload, Folder } from "lucide-react";
+import { CloudDownload, Folder, Settings } from "lucide-react";
+
 import { Button } from "../ui/button";
 
 export function Footer() {
   return (
-    <footer className="flex flex-row items-center gap-2 justify-start">
-      <Button variant="ghost">
-        <CloudDownload />
-      </Button>
-      <Button variant="ghost">
+    <footer className="flex flex-row items-center gap-1 justify-start p-2">
+      <div className="flex flex-row items-center justify-start w-full text-sm">
+        <CloudDownload className="mr-2 size-5" />
+        CloudDown
+      </div>
+
+      <Button
+        variant="ghost"
+        size="lg"
+        className="hover:bg-zinc-900/70 hover:text-white transition-all duration-200"
+      >
         <Folder />
+      </Button>
+
+      <Button
+        variant="ghost"
+        size="lg"
+        className="hover:bg-zinc-900/70 hover:text-white transition-all duration-200"
+      >
+        <Settings />
       </Button>
     </footer>
   );
